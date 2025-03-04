926576d6cb976f1c2f22942a26dfa8b082d1ce93
SavioSReis <hjd44.sa@gmail.com>
2025-03-04 17:52:08 -0300

    adicionando conexão com o MongoDB no servidor Apollo

--- src/startServer.js
+++ src/startServer.js
@@ -1,6 +1,9 @@
 import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
 
 function startServer({ typeDefs, resolvers }) {
+    mongoose.connect('mongodb://localhost:27017/graphql')
+
     const server = new ApolloServer({ typeDefs, resolvers });
     server.listen().then(({ url }) => console.log(`🚀 Server ready at ${url}`));
 }
