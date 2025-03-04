49c43c1b57b3bcc551da1519c9962726cacdd24e
SavioSReis <hjd44.sa@gmail.com>
2025-03-04 17:21:28 -0300

    adicionando função para iniciar o servidor Apollo com typeDefs e resolvers

---
+++ src/startServer.js
@@ -0,0 +1,8 @@
+import { ApolloServer } from 'apollo-server';
+
+function startServer({ typeDefs, resolvers }) {
+    const server = new ApolloServer({ typeDefs, resolvers });
+    server.listen().then(({ url }) => console.log(`🚀 Server ready at ${url}`));
+}
+
+export default startServer;
