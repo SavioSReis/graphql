d91e75335df9d0be9bd0cbb8e9a70a2393cd421b
SavioSReis <hjd44.sa@gmail.com>
2025-03-04 16:45:50 -0300

    adicionando consultas e mutações para gerenciamento de usuários no esquema GraphQL

--- src/index.js
+++ src/index.js
@@ -20,30 +20,49 @@ const typeDefs = gql`
     type Query {
         hello: String
         getUsers: [User!]!
+        getUserByEmail(email: String): User
+    }
+
+    type Mutation {
+        createUser(name: String!, phone_number: String!, email: String): User!
     }
 `;
+
+const users = [
+    {
+        _id: String(Math.random()),
+        name: 'John Doe',
+        email: null,
+        phone_number: '+1234567890',
+        balance: (Math.random() * 1000).toFixed(2),
+        is_active: true
+    },
+    {
+        _id: String(Math.random()),
+        name: 'Jane Doe',
+        email: 'jane@dev.com',
+        phone_number: '+0987654321',
+        balance: (Math.random() * 1000).toFixed(2),
+        is_active: false
+    },
+];
 
 const resolvers = {
     Query: {
         hello: () => 'Hello world!',
-        users: () => [
-            {
-                _id: String(Math.random()),
-                name: 'John Doe',
-                email: null,
-                phone_number: '+1234567890',
-                balance: Math.random() * 1000,
-                is_active: true
-            },
-            {
-                _id: String(Math.random()),
-                name: 'Jane Doe',
-                email: 'jane@dev.com',
-                phone_number: '+0987654321',
-                balance: Math.random() * 1000,
-                is_active: false
-            },
-        ]
+        getUsers: () => users,
+        getUserByEmail: (_, { email }) => users.find(user => user.email === email)
+    },
+
+    Mutation: {
+        createUser: (_, args) => ({
+            _id: String(Math.random()),
+            name: args.name,
+            email: args.email || null,
+            phone_number: args.phone_number,
+            balance: (Math.random() * 1000).toFixed(2),
+            is_active: true
+        })
     }
 };
 
