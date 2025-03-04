2cd041f6818224c8ad5fa762cd51b323ede71268
SavioSReis <hjd44.sa@gmail.com>
2025-03-04 16:22:17 -0300

    adicionando tipos e resolvers para usuários e posts no esquema GraphQL

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import { ApolloServer, gql } from 'apollo-server'
 
 const typeDefs = gql`
+    type User {
+        _id: ID!
+        name: String!
+        email: String
+        phone_number: String!
+        balance: Float!
+        is_active: Boolean!
+    }
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String
+        author: User!
+    }
+
     type Query {
         hello: String
+        users: [User!]!
     }
 `;
+
 const resolvers = {
     Query: {
         hello: () => 'Hello world!',
+        users: () => [
+            {
+                _id: String(Math.random()),
+                name: 'John Doe',
+                email: null,
+                phone_number: '+1234567890',
+                balance: Math.random() * 1000,
+                is_active: true
+            },
+            {
+                _id: String(Math.random()),
+                name: 'Jane Doe',
+                email: 'jane@dev.com',
+                phone_number: '+0987654321',
+                balance: Math.random() * 1000,
+                is_active: false
+            },
+        ]
     }
 };
 
